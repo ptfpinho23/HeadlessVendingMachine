afe7dcb05e26afbaba0a67339dbdfc17274a558f
Pedro Pinho <ptfpinho@hotmail.com>
2023-01-20 11:34:11 +0100

    Add nest bootstrap

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('headless Vending Machine API')
+    .setDescription('A Simple headless Vending Machine API Implementation')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-documentation', app, document);
+
+  await app.listen(3000);
+}
+bootstrap();
