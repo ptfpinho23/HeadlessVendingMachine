beb2765836e932886b18a5f04d667bb11267b3d7
Pedro Pinho <ptfpinho@hotmail.com>
2023-01-20 16:07:46 +0100

    Update main app module
    - Inject native cache module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
-import { Module } from '@nestjs/common';
+import { Module, CacheModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import * as redisStore from 'cache-manager-redis-store';
 
 @Module({
-  imports: [],
+  imports: [CacheModule.register({
+    store: redisStore,
+    host: process.env.HOST ?? 'localhost',
+    port: process.env.PORT ?? 6379
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
