d4b476c89dfff14cc540ea833de6fefdbae799f3
Pedro Pinho <ptfpinho@hotmail.com>
2023-01-20 12:00:29 +0100

    Add initial prisma schema + migration scaffold

---
+++ prisma/migrations/20230120105448_init/migration.sql
@@ -0,0 +1,55 @@
+-- CreateEnum
+CREATE TYPE "Roles" AS ENUM ('buyer', 'seller');
+
+-- CreateEnum
+CREATE TYPE "Status" AS ENUM ('pending', 'paid', 'expired', 'failed');
+
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" TEXT NOT NULL,
+    "amount_available" INTEGER NOT NULL,
+    "cost" INTEGER NOT NULL,
+    "product_name" VARCHAR(50) NOT NULL,
+    "date_created" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "date_updated" TIMESTAMP(3) NOT NULL,
+    "sellerId" TEXT NOT NULL,
+
+    CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "user" (
+    "id" TEXT NOT NULL,
+    "username" VARCHAR(50) NOT NULL,
+    "password" VARCHAR(1000) NOT NULL,
+    "deposits" INTEGER NOT NULL DEFAULT 0,
+    "role" "Roles" NOT NULL,
+    "date_created" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "date_updated" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "user_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Ledger" (
+    "id" TEXT NOT NULL,
+    "productId" TEXT NOT NULL,
+    "date_created" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "delta_value" INTEGER NOT NULL,
+    "operation_source" VARCHAR(50) NOT NULL,
+    "status" "Status" NOT NULL DEFAULT 'pending',
+
+    CONSTRAINT "Ledger_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Product_product_name_key" ON "Product"("product_name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "user_username_key" ON "user"("username");
+
+-- AddForeignKey
+ALTER TABLE "Product" ADD CONSTRAINT "Product_sellerId_fkey" FOREIGN KEY ("sellerId") REFERENCES "user"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Ledger" ADD CONSTRAINT "Ledger_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
