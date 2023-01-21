b72d505d5dc520c50068041065cdc19bb6182753
Pedro Pinho <ptfpinho@hotmail.com>
2023-01-21 10:35:06 +0100

    Update configs

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,8 +1,6 @@
-import 'dotenv/config';
-
 export const config = {
-  databaseUrl: process.env.DATABASE_UR!,
-  jwtSecret: process.env.JWT_SECRET!,
+  databaseUrl: process.env.DATABASE_URL,
+  jwtSecret: process.env.JWT_SECRET,
   joiOptions: {
     errors: {
       wrap: { label: '' },
