f2e3b116eafb541e683fdec931512974ead241c4
Pedro Pinho <ptfpinho@hotmail.com>
2023-01-21 13:23:33 +0100

    Update jwt guard

--- src/v1/auth/guards/jwt.guard.ts
+++ src/v1/auth/guards/jwt.guard.ts
@@ -1,23 +1,44 @@
-import { CACHE_MANAGER, ExecutionContext, Inject, Injectable } from '@nestjs/common';
+import {
+  CACHE_MANAGER,
+  ExecutionContext,
+  Inject,
+  Injectable,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { AuthGuard } from '@nestjs/passport';
 import { Cache } from 'cache-manager';
+import { jwtPayload } from '../../../common/interfaces';
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
-    constructor(@Inject(CACHE_MANAGER) private cacheService: Cache) {
-        super()
+  constructor(
+    @Inject(CACHE_MANAGER) private cacheService: Cache,
+    private readonly jwtService: JwtService,
+  ) {
+    super();
+  }
+
+  async canActivate(context: ExecutionContext): Promise<any> {
+    const request = context.switchToHttp().getRequest();
+    const authToken = request.headers.authorization;
+
+    if (!authToken) {
+      return false;
+    }
+    const jwt = authToken.split(' ')[1];
+    const decodedJwt = this.jwtService.decode(jwt);
+
+    if (!decodedJwt) {
+      return false;
     }
 
-    async canActivate(context: ExecutionContext): Promise<any> {
+    // Check for cached user session
+    const cachedSessions = (await this.cacheService.get(
+      (decodedJwt as jwtPayload).username,
+    )) as string;
 
-        const request = context.switchToHttp().getRequest()
-        const authToken = request.headers.authorization
-        const jwt = authToken.split(" ")[1]
+    if (!cachedSessions) return false;
 
-        // Check for cached sessions
-        const cachedSessions = await this.cacheService.get(`*${jwt}*`) as string[]
-        if (cachedSessions.length === 0) return false
-
-        return super.canActivate(context);
-    }
+    return super.canActivate(context);
+  }
 }
