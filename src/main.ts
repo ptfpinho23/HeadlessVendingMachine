a6271b05f3524ee97bd072464e99dbcb1c3af33f
Pedro Pinho <ptfpinho@hotmail.com>
2023-01-21 12:51:32 +0100

    Update bootstraping
    - Properly Inject server port global vars with fallback

--- src/main.ts
+++ src/main.ts
@@ -11,8 +11,8 @@ async function bootstrap() {
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('/api/v1', app, document);
+  SwaggerModule.setup('/api/v1/docs', app, document);
 
-  await app.listen(3000);
+  await app.listen((process.env.SERVER_PORT as string) ?? 8000);
 }
 bootstrap();
