11ab19c7e596096ef96fa1df7b1ec34871835018
Pedro Pinho <ptfpinho@hotmail.com>
2023-01-22 10:41:45 +0100

    Update root app module
    - Global cache module injection
    - App modules injection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,28 @@
-import { Module, CacheModule } from '@nestjs/common';
+import { CacheModule, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './v1/auth/auth.module';
+import { UsersModule } from './v1/users/users.module';
+import { ProductsModule } from './v1/products/products.module';
+import { PurchaseModule } from './v1/purchase/purchase.module';
+import * as redisStore from 'cache-manager-redis-store';
+import { SESSION_TTL } from './common/constants';
+
 @Module({
-  imports: [CacheModule.register({
-    host: process.env.HOST ?? 'localhost',
-    port: process.env.PORT ?? 6379,
-    isGlobal: true
-  })],
+  imports: [
+    AuthModule,
+    UsersModule,
+    ProductsModule,
+    PurchaseModule,
+    CacheModule.register({
+      store: redisStore,
+      host: process.env.REDIS_HOST,
+      port: process.env,
+      ttl: SESSION_TTL,
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule { }
+export class AppModule {}
